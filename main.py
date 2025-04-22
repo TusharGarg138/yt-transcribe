e80d9579fb0045a53954d46bacb27a008a2d5724
Tushar Garg <144510935+TusharGarg138@users.noreply.github.com>
2025-04-22 22:58:41 +0530

    importing lib

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import os
+import re
+import torch
+from yt_dlp import YoutubeDL
+from moviepy.editor import VideoFileClip
+import whisper
+from transformers import BartTokenizer, BartForConditionalGeneration
+import gradio as gr
+from datetime import datetime
+from gtts import gTTS
